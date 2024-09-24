3c3021f4bd897c95bf965588a0c72cfe7d78b964
Luiz Clovis Dos Santos Matias <luizclovis2014@gmail.com>
2024-09-24 15:31:56 -0300

    Realizado a criação de duas novas api(Calculator e person com dados estáticos)

---
+++ 03_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -0,0 +1,23 @@
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
