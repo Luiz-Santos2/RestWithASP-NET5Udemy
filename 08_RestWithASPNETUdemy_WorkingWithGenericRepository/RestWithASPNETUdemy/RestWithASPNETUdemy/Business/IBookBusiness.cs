0d4ecdc80a4c6fba2fe8b78d3c324d35202e5a30
Luiz Clovis Dos Santos Matias <luizclovis2014@gmail.com>
2024-09-25 18:29:54 -0300

    Reallizado a implementação do padrão de projetos generic repository

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IBookBusiness.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IBookBusiness
+    {
+        Book Create(Book book);
+        Book FindByID(long id);
+        List<Book> FindAll();
+        Book Update(Book book);
+        void Delete(long id);
+    }
+}
---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
