a706ee968646202cb8674a6a7b2a4fb0c0ab8b53
Luiz Clovis Dos Santos Matias <luizclovis2014@gmail.com>
2024-09-26 15:46:37 -0300

    Realizado a implementação do content negociation

---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IBookBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindByID(long id);
+        List<BookVO> FindAll();
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+        public string? author { get; set; }
+        public DateTime? date { get; set; }
+        public Decimal? price { get; set; }
+        public string? title { get; set; }
+    }
+}
