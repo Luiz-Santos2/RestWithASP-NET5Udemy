6b7d650838a516089e5b64e9e1ee2e0d234d3867
Luiz Clovis Dos Santos Matias <luizclovis2014@gmail.com>
2024-09-26 12:54:16 -0300

    Implementado custom serialization

---
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BookVO
+    {
+        [JsonPropertyName("code")]
+        public long Id { get; set; }
+        [JsonPropertyName("author")]
+        public string? author { get; set; }
+        [JsonIgnore]
+        public DateTime? date { get; set; }
+        [JsonPropertyName("price")]
+        public Decimal? price { get; set; }
+        [JsonPropertyName("title")]
+        public string? title { get; set; }
+    }
+}
---
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Context/MySqlContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNETUdemy.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() 
+        {
+
+        }
+        public MySqlContext(DbContextOptions<MySqlContext> options): base(options) 
+        {
+        
+        }
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Book> Books { get; set; }
+    }
+}
